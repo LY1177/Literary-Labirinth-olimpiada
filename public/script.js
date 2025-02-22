0e7fac59bcdae5716356114fdad176144d3dad51
LY1177 <lpgamingg83@gmail.com>
2025-02-22 22:29:03 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -252,7 +252,7 @@ function getQuestionsForAuthor(authorName, callback) {
   //fetch(`http://localhost:3000/api/questions?author=${encodeURIComponent(authorName)}`)
   
    //fetch("https://literary-labirinth-olimpiada.onrender.com" + encodeURIComponent(authorName))
-   fetch("https://literary-labirinth-olimpiada.onrender.com//api/questions?author=" + encodeURIComponent(authorName))
+   fetch("https://literary-labirinth-olimpiada.onrender.com/api/questions?author=" + encodeURIComponent(authorName))
     .then(r => r.json())
     .then(data => callback(data))
     .catch(err => console.error("Грешка при извличане на въпроси:", err));
