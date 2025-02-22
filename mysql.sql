46edf04a41a71e175c975fa8568f75dc555f7564
LY1177 <lpgamingg83@gmail.com>
2025-02-22 18:13:35 +0200

    Първи комит (Initial commit)

---
+++ mysql.sql
@@ -0,0 +1,1 @@
+INSERT INTO authors (id, name) VALUES (12, 'НВО');                                                                                                                                                                                                                                                                                        
